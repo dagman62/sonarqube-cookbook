4e961a97292b3900539f167bfd80724c9d3be012
Gaetano Giacalone <dagman62@gmail.com>
2018-08-24 15:49:38 -0700

    added installation and configuration of postgres for sonarqube

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,44 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+platform = node['platform']
+
+if platform == 'ubuntu' || platform == 'debian'
+  package %w(postgresql postgresql-client) do 
+    action :install
+  end
+elsif platform == 'centos' || platform == 'fedora'
+  package %w(postgresql postgresql-server) do 
+    action :install
+  end
+else
+  log 'This platform is not supported for this cookbook' do
+    level :info
+  end
+  exit
+end
+
+execute 'Initialize The Database' do
+  command 'postgresql-setup initdb | tee -a /tmp/db-initialized'
+  action :run
+  not_if { File.exist?('/tmp/db-initialized') }
+end
+
+service 'postgresql' do
+  action [:start, :enable]
+end
+
+bash 'Create Database User and Grants' do
+  code <<-EOH
+  su - postgres
+  psql -c "create database sonar"
+  psql -c "create user sonar with password 'sonar'"
+  psql -c "grant all privileges on database sonar to sonar"
+  EOH
+  action :run
+end
+
+
+
+
